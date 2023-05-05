e1b3b34d83e882704e18cda8cfec344f2cab2b3e
Said Kayed <saka@quranload.com>
2023-05-05 15:44:33 +0200

    changes

--- components/Screens/GameMap.tsx
+++ components/Screens/GameMap.tsx
@@ -1,278 +1,79 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Animated, View } from "react-native";
 import Spawner, { Location } from "../../api/Spawner";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
 import * as PhoneLocation from "expo-location";
 import * as Permissions from "expo-permissions";
+import {mapStyle} from "../../global/GoogleMapStyle"
 
 export default function GameMap({ path }: { path: string }) {
-  //#region mapstyle
-  const mapStyle = [
-    {
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#1d2c4d",
-        },
-      ],
-    },
-    {
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#8ec3b9",
-        },
-      ],
-    },
-    {
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#1a3646",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.country",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#4b6878",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.land_parcel",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#64779e",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.province",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#4b6878",
-        },
-      ],
-    },
-    {
-      featureType: "landscape.man_made",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#334e87",
-        },
-      ],
-    },
-    {
-      featureType: "landscape.natural",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#023e58",
-        },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#283d6a",
-        },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#6f9ba5",
-        },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#1d2c4d",
-        },
-      ],
-    },
-    {
-      featureType: "poi.park",
-      elementType: "geometry.fill",
-      stylers: [
-        {
-          color: "#023e58",
-        },
-      ],
-    },
-    {
-      featureType: "poi.park",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#3C7680",
-        },
-      ],
-    },
-    {
-      featureType: "road",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#304a7d",
-        },
-      ],
-    },
-    {
-      featureType: "road",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#98a5be",
-        },
-      ],
-    },
-    {
-      featureType: "road",
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#1d2c4d",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#2c6675",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#255763",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#b0d5ce",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#023e58",
-        },
-      ],
-    },
-    {
-      featureType: "transit",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#98a5be",
-        },
-      ],
-    },
-    {
-      featureType: "transit",
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#1d2c4d",
-        },
-      ],
-    },
-    {
-      featureType: "transit.line",
-      elementType: "geometry.fill",
-      stylers: [
-        {
-          color: "#283d6a",
-        },
-      ],
-    },
-    {
-      featureType: "transit.station",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#3a4762",
-        },
-      ],
-    },
-    {
-      featureType: "water",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#0e1626",
-        },
-      ],
-    },
-    {
-      featureType: "water",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#4e6d70",
-        },
-      ],
-    },
-  ];
-  //#endregion
+
 
   const [locations, setLocations] = useState<Location[]>([]);
   const [errorMSG, setErrorMSG] = useState<string>("");
-  
+  const [userLocation, setUserLocation] = useState<Location>( {lat: 0, long: 0} as Location);
+  const [markerScale] = useState(new Animated.Value(1));
+
   useEffect(() => {
-    const spawner = new Spawner("https://97f9-89-23-224-32.ngrok-free.app");
   
-    const getLocation = async () => {
-      try {
-        const { status } = await Permissions.askAsync(Permissions.LOCATION_BACKGROUND);
-        if (status !== "granted") {
-          setErrorMSG("Permission to access location was denied");
-          return;
-        }
-  
-        const userLocation = await PhoneLocation.getCurrentPositionAsync();
-        const { latitude, longitude } = userLocation.coords;
-        const fetchedLocations = await spawner.getLocation(latitude, longitude);
-        setLocations(fetchedLocations);
-      } catch (error) {
-        console.error("Error fetching location:", error);
-        setErrorMSG("Error fetching location");
-      }
-    };
+
+    //get current location and update every 5 seconds
+    
+    setInterval(async () => {
+      const userLocation = await PhoneLocation.getCurrentPositionAsync();
+
+      setUserLocation({lat: userLocation.coords.latitude, long: userLocation.coords.longitude});
+
+    }, 5000);
+   
   
     getLocation();
+    pulseMarker();
   }, []);
+
+  const getLocation = async () => {
+    try {
+      const spawner = new Spawner("https://c904-89-23-224-32.ngrok-free.app");
+
+      const { status } = await Permissions.askAsync(Permissions.LOCATION_BACKGROUND);
+      if (status !== "granted") {
+        setErrorMSG("Permission to access location was denied");
+        return;
+      }
+
+      const userLocation = await PhoneLocation.getCurrentPositionAsync();
+      const { latitude, longitude } = userLocation.coords;
+      const fetchedLocations = await spawner.getLocation(latitude, longitude);
+      setLocations(fetchedLocations);
+    } catch (error) {
+      console.error("Error fetching location:", error);
+      setErrorMSG("Error fetching location");
+    }
+  };
+
+// Animation for marker icon
+const pulseMarker = () => {
+  Animated.sequence([
+    Animated.timing(markerScale, {
+      toValue: 1.2,
+      duration: 300,
+      useNativeDriver: true,
+    }),
+    Animated.timing(markerScale, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }),
+  ]).start(() => pulseMarker());
+};
+
+
 
   return (
     <MapView style={styles.map} customMapStyle={mapStyle}>
+
       {locations.map((location, index) => (
         <Marker
           key={index}
@@ -286,6 +87,17 @@ export default function GameMap({ path }: { path: string }) {
           <MaterialIcons name="shield" size={30} color="red" />
         </Marker>
       ))}
+
+      <Marker
+      focusable={true}
+      coordinate={{
+        latitude:userLocation.lat, 
+        longitude:userLocation.long}}
+      >
+     <Animated.View style={{ transform: [{ scale: markerScale }] }}>
+          <MaterialIcons name="android" size={35} color="white"/>
+        </Animated.View>
+      </Marker>
     </MapView>
   );
 }
